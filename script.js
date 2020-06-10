a0ec952a202fad1bd48baad3687cf8688327b407
Chris Storrs-Fox <cmstorrsfox@gmail.com>
2020-06-10 22:36:04 +0700

    sticky navbar added with JS

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 const navbar = document.getElementById('navbar');
+const header = document.getElementById('heading');
+const sticky = navbar.offsetTop;
+
+window.onscroll = function() {
+    myFunction();
+
+};
+
+function myFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+
+    }
+
+};
